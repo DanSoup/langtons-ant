4a504d558a2bcaf5ac033277b3c29bc6c472456f
Dan Soup <dansoup@hotmail.co.uk>
2022-02-22 14:58:55 +0000

    updating public path for react router

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import MainPage from './components/MainPage/MainPage.js'
 
 class App extends Component {
   render () {
+    console.log(process.env.PUBLIC_PATH)
     // return <BrowserRouter basename={`/${process.env.BS_CONFIG.BS_FOLDER}/${process.env.BS_CONFIG.BS_URL}`}>
-    return <BrowserRouter>
+    return <BrowserRouter basename={process.env.PUBLIC_PATH}>
       <MainPage/>
     </BrowserRouter>
   };
