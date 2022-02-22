156412cf2d49e88a06251b93b95d83625e0f4790
Dan Soup <dansoup@hotmail.co.uk>
2022-02-22 13:48:57 +0000

    updating public path for live

--- src/components/Ant/Ant.js
+++ src/components/Ant/Ant.js
@@ -3,6 +3,19 @@ import './Ant.scss';
 
 function Ant() {
   return <div id="ant" className="page">
+    {/* Main Body */}
+    <div id="main-body">
+
+      {/* Control Menu */}
+      <div></div>
+
+      {/* Canvas */}
+      <div></div>
+
+      {/* Export Menu */}
+      <div></div>
+    </div>
+
     <canvas></canvas>
   </div>;
 };
